5141717609b1356318db67baf07d2ddc069d89f0
agavrikov <agavrikov>
2020-03-22 20:22:35 +0300

    move code from controller to service

--- src/main/java/warehouse_planning/controller/WarehouseController.java
+++ src/main/java/warehouse_planning/controller/WarehouseController.java
@@ -1,26 +1,16 @@
 package warehouse_planning.controller;
 
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.PushbackInputStream;
 import java.time.*;
-import java.util.ArrayList;
-import java.util.List;
 
 import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
-import org.apache.poi.openxml4j.opc.OPCPackage;
-import org.apache.poi.xssf.usermodel.XSSFRow;
-import org.apache.poi.xssf.usermodel.XSSFSheet;
-import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.HttpStatus;
-import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.ModelAndView;
 
-import warehouse_planning.model.Slot;
 import warehouse_planning.service.SlotService;
 import warehouse_planning.service.WarehouseService;
 
@@ -30,9 +20,6 @@ import warehouse_planning.service.WarehouseService;
 @RestController
 @RequestMapping("/warehouses/")
 public class WarehouseController {
-
-    private static final int LIMIT = 10;
-    private static final int PAGE_COUNT = 10;
 
     private final SlotService slotService;
     private final WarehouseService warehouseService;
@@ -52,67 +39,8 @@ public class WarehouseController {
         @RequestParam(required = false) @DateTimeFormat(iso = DateTimeFormat.ISO.TIME) LocalTime timeFrom,
         @RequestParam(required = false) @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate dateTo,
         @RequestParam(required = false) @DateTimeFormat(iso = DateTimeFormat.ISO.TIME) LocalTime timeTo,
-        @RequestParam(required = false, defaultValue = "1") int page)
-    {
-        String viewName = "";
-        LocalDateTime dateTimeFrom = null;
-        LocalDateTime dateTimeTo = null;
-        LocalTime midnight = LocalTime.of(0, 0);
-
-        if (StringUtils.isEmpty(guid) && conveyorId == null && date == null &&
-                dateFrom == null && timeFrom == null &&
-                dateTo == null && timeTo == null) {
-            viewName = "slots";
-        } else if (conveyorId != null && StringUtils.isEmpty(guid) && date == null &&
-                dateFrom == null && timeFrom == null &&
-                dateTo == null && timeTo == null) {
-            viewName = "conveyor";
-        } else if (date != null && StringUtils.isEmpty(guid) && conveyorId == null &&
-                dateFrom == null && timeFrom == null &&
-                dateTo == null && timeTo == null) {
-            viewName = "date";
-            dateTimeFrom = LocalDateTime.of(date, midnight);
-            dateTimeTo = LocalDateTime.of(date.plusDays(1), midnight);
-        } else {
-            viewName = "search";
-            if (dateFrom != null) {
-                dateTimeFrom = LocalDateTime.of(dateFrom, timeFrom != null ? timeFrom : midnight);
-            }
-            if (dateTo != null) {
-                dateTimeTo = timeTo != null ? LocalDateTime.of(dateTo, timeTo) : LocalDateTime.of(dateTo.plusDays(1), midnight);
-            }
-        }
-
-        ModelAndView model = new ModelAndView("warehouse_planning/" + viewName);
-        model.addObject("warehouseId", id);
-        model.addObject("guid", guid);
-        model.addObject("conveyorId", conveyorId);
-        model.addObject("date", date);
-        model.addObject("dateFrom", dateFrom);
-        model.addObject("timeFrom", timeFrom);
-        model.addObject("dateTo", dateTo);
-        model.addObject("timeTo", timeTo);
-        model.addObject("slots", slotService.getSlots(id, guid, conveyorId, dateTimeFrom, dateTimeTo, page, LIMIT));
-        int maxPage = (int) Math.ceil(
-                slotService.getSlotCount(id, guid, conveyorId, dateTimeFrom, dateTimeTo) * 1. / LIMIT);
-
-        List<Integer> pages = new ArrayList<>();
-        int left, right;
-        if (page - PAGE_COUNT / 2 < 1) {
-            left = 1;
-        } else if (page + PAGE_COUNT / 2 + 1 > maxPage) {
-            left = Math.max(maxPage - PAGE_COUNT + 1, 1);
-        } else {
-            left = page - PAGE_COUNT / 2;
-        }
-        right = Math.min(left + PAGE_COUNT, maxPage);
-        for (int p = left; p <= right; ++p) {
-            pages.add(p);
-        }
-        model.addObject("page", page);
-        model.addObject("maxPage", maxPage);
-        model.addObject("pages", pages);
-        return model;
+        @RequestParam(required = false, defaultValue = "1") int page) {
+        return slotService.getSlotModelAndView(guid, id, conveyorId, date, dateFrom, timeFrom, dateTo, timeTo, page);
     }
 
     @GetMapping(value = "{id}/planning")
@@ -128,8 +56,7 @@ public class WarehouseController {
     }
 
     @PostMapping(value = "/")
-    public ModelAndView add(
-            @RequestParam String name) {
+    public ModelAndView add(@RequestParam String name) {
         warehouseService.save(name);
         return new ModelAndView("redirect:/warehouses/");
     }
@@ -144,47 +71,10 @@ public class WarehouseController {
     }
 
     @PostMapping(value = "{id}/download")
-    public ModelAndView download(@PathVariable long id,
-                                 @RequestParam MultipartFile file) throws IOException, InvalidFormatException {
-
-        InputStream inputStream = file.getInputStream();
-        if (!inputStream.markSupported()) {
-            inputStream = new PushbackInputStream(inputStream, 8);
-        }
-        XSSFWorkbook workbook = new XSSFWorkbook(OPCPackage.open(inputStream));
-        XSSFSheet sheet = workbook.getSheetAt(0);
-
-        List<Slot> slots = new ArrayList<>();
-
-        for (int i = 0; i < sheet.getLastRowNum(); ++i) {
-            XSSFRow row = sheet.getRow(i);
-            String guid = row.getCell(0).getStringCellValue();
-
-            LocalDate date = getLocalDateCell(row, 1);
-            LocalTime timeFrom = getLocalTimeCell(row, 2);
-            LocalTime timeTo = getLocalTimeCell(row, 3);
-
-            if (timeFrom.isAfter(timeTo) || timeFrom.equals(timeTo)) {
-                continue;
-            }
-
-            slots.add(new Slot(0, guid, id, date, timeFrom, timeTo, -1, LocalDateTime.now()));
-        }
-        slotService.saveBatch(slots);
+    public ModelAndView download(@PathVariable long id, @RequestParam MultipartFile file)
+            throws IOException, InvalidFormatException {
+        slotService.download(id, file);
         return new ModelAndView("redirect:/warehouses/" + id + "/slots");
-    }
-
-    private LocalDate getLocalDateCell(XSSFRow row, int cellnum) {
-        return Instant.ofEpochMilli(
-            row.getCell(cellnum).getDateCellValue().getTime()
-        ).atZone(ZoneId.systemDefault()).toLocalDate();
-    }
-
-    private LocalTime getLocalTimeCell(XSSFRow row, int cellnum) {
-        LocalTime dateTime = LocalDateTime.ofInstant(
-            row.getCell(cellnum).getDateCellValue().toInstant(), ZoneId.systemDefault()
-        ).toLocalTime();
-        return LocalTime.of(dateTime.getHour(), dateTime.getMinute());
     }
 
     @DeleteMapping(value = "{id}")
--- src/main/java/warehouse_planning/service/SlotService.java
+++ src/main/java/warehouse_planning/service/SlotService.java
@@ -1,18 +1,27 @@
 package warehouse_planning.service;
 
-import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.time.LocalTime;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.PushbackInputStream;
+import java.time.*;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
+import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
+import org.apache.poi.openxml4j.opc.OPCPackage;
+import org.apache.poi.xssf.usermodel.XSSFRow;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.stereotype.Component;
 
+import org.springframework.util.StringUtils;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.ModelAndView;
 import warehouse_planning.algorithm.WarehousePlanning;
 import warehouse_planning.mapper.SlotMapper;
 import warehouse_planning.model.Slot;
@@ -24,6 +33,9 @@ import warehouse_planning.model.Slot;
 public class SlotService {
 
     private static final String SEQUENCE = "slot_seq";
+
+    private static final int LIMIT = 10;
+    private static final int PAGE_COUNT = 10;
 
     private final NamedParameterJdbcTemplate jdbcTemplate;
 
@@ -118,6 +130,33 @@ public class SlotService {
         jdbcTemplate.update(sql, new MapSqlParameterSource().addValue("id", id));
     }
 
+    public void download(long warehouseId, MultipartFile file) throws IOException, InvalidFormatException {
+        InputStream inputStream = file.getInputStream();
+        if (!inputStream.markSupported()) {
+            inputStream = new PushbackInputStream(inputStream, 8);
+        }
+        XSSFWorkbook workbook = new XSSFWorkbook(OPCPackage.open(inputStream));
+        XSSFSheet sheet = workbook.getSheetAt(0);
+
+        List<Slot> slots = new ArrayList<>();
+
+        for (int i = 0; i < sheet.getLastRowNum(); ++i) {
+            XSSFRow row = sheet.getRow(i);
+            String guid = row.getCell(0).getStringCellValue();
+
+            LocalDate date = getLocalDateCell(row, 1);
+            LocalTime timeFrom = getLocalTimeCell(row, 2);
+            LocalTime timeTo = getLocalTimeCell(row, 3);
+
+            if (timeFrom.isAfter(timeTo) || timeFrom.equals(timeTo)) {
+                continue;
+            }
+
+            slots.add(new Slot(0, guid, warehouseId, date, timeFrom, timeTo, -1, LocalDateTime.now()));
+        }
+        saveBatch(slots);
+    }
+
     public void updateConveyors(List<Slot> slots) {
         String sql = "UPDATE slot SET conveyor_id = :conveyorId WHERE id = :id";
         List<Map<String, Object>> batchValues = new ArrayList<>(slots.size());
@@ -147,6 +186,80 @@ public class SlotService {
             .collect(Collectors.toList());
         WarehousePlanning.planning(slots);
         updateConveyors(slots);
+    }
+
+    public ModelAndView getSlotModelAndView(String guid, long warehouseId, Integer conveyorId, LocalDate date,
+        LocalDate dateFrom, LocalTime timeFrom, LocalDate dateTo, LocalTime timeTo, int page)
+    {
+        String viewName = "";
+        LocalDateTime dateTimeFrom = null;
+        LocalDateTime dateTimeTo = null;
+        LocalTime midnight = LocalTime.of(0, 0);
+
+        if (StringUtils.isEmpty(guid) && conveyorId == null && date == null && dateFrom == null && timeFrom == null &&
+                dateTo == null && timeTo == null) {
+            viewName = "slots";
+        } else if (conveyorId != null && StringUtils.isEmpty(guid) && date == null && dateFrom == null &&
+                timeFrom == null && dateTo == null && timeTo == null) {
+            viewName = "conveyor";
+        } else if (date != null && StringUtils.isEmpty(guid) && conveyorId == null && dateFrom == null &&
+                timeFrom == null && dateTo == null && timeTo == null) {
+            viewName = "date";
+            dateTimeFrom = LocalDateTime.of(date, midnight);
+            dateTimeTo = LocalDateTime.of(date.plusDays(1), midnight);
+        } else {
+            viewName = "search";
+            if (dateFrom != null) {
+                dateTimeFrom = LocalDateTime.of(dateFrom, timeFrom != null ? timeFrom : midnight);
+            }
+            if (dateTo != null) {
+                dateTimeTo = timeTo != null ? LocalDateTime.of(dateTo, timeTo) : LocalDateTime.of(dateTo.plusDays(1), midnight);
+            }
+        }
+
+        ModelAndView model = new ModelAndView("warehouse_planning/" + viewName);
+        model.addObject("warehouseId", warehouseId);
+        model.addObject("guid", guid);
+        model.addObject("conveyorId", conveyorId);
+        model.addObject("date", date);
+        model.addObject("dateFrom", dateFrom);
+        model.addObject("timeFrom", timeFrom);
+        model.addObject("dateTo", dateTo);
+        model.addObject("timeTo", timeTo);
+        model.addObject("slots", getSlots(warehouseId, guid, conveyorId, dateTimeFrom, dateTimeTo, page, LIMIT));
+        int maxPage = (int) Math.ceil(
+                getSlotCount(warehouseId, guid, conveyorId, dateTimeFrom, dateTimeTo) * 1. / LIMIT);
+
+        List<Integer> pages = new ArrayList<>();
+        int left, right;
+        if (page - PAGE_COUNT / 2 < 1) {
+            left = 1;
+        } else if (page + PAGE_COUNT / 2 + 1 > maxPage) {
+            left = Math.max(maxPage - PAGE_COUNT + 1, 1);
+        } else {
+            left = page - PAGE_COUNT / 2;
+        }
+        right = Math.min(left + PAGE_COUNT, maxPage);
+        for (int p = left; p <= right; ++p) {
+            pages.add(p);
+        }
+        model.addObject("page", page);
+        model.addObject("maxPage", maxPage);
+        model.addObject("pages", pages);
+        return model;
+    }
+
+    private LocalDate getLocalDateCell(XSSFRow row, int cellnum) {
+        return Instant.ofEpochMilli(
+                row.getCell(cellnum).getDateCellValue().getTime()
+        ).atZone(ZoneId.systemDefault()).toLocalDate();
+    }
+
+    private LocalTime getLocalTimeCell(XSSFRow row, int cellnum) {
+        LocalTime dateTime = LocalDateTime.ofInstant(
+                row.getCell(cellnum).getDateCellValue().toInstant(), ZoneId.systemDefault()
+        ).toLocalTime();
+        return LocalTime.of(dateTime.getHour(), dateTime.getMinute());
     }
 
     @Autowired
