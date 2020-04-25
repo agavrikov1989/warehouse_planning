a5a81dbcc121660993b3dd9d57116fe41e53bca5
agavrikov <agavrikov>
2020-04-25 14:11:09 +0300

    add application.properties

--- src/main/java/warehouse_planning/config/JdbcConfig.java
+++ src/main/java/warehouse_planning/config/JdbcConfig.java
@@ -2,6 +2,7 @@ package warehouse_planning.config;
 
 import javax.sql.DataSource;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
@@ -13,13 +14,25 @@ import org.springframework.jdbc.datasource.DriverManagerDataSource;
 @Configuration
 @ComponentScan("warehouse_planning")
 public class JdbcConfig {
+
+    @Value("${host}")
+    private String host;
+    @Value("${port}")
+    private int port;
+    @Value("${database}")
+    private String database;
+    @Value("${username}")
+    private String username;
+    @Value("${password}")
+    private String password;
+
     @Bean
     public DataSource dataSource() {
         DriverManagerDataSource dataSource = new DriverManagerDataSource();
         dataSource.setDriverClassName("org.postgresql.Driver");
-        dataSource.setUrl("jdbc:postgresql://localhost:5432/postgres");
-        dataSource.setUsername("postgres");
-        dataSource.setPassword("postgres");
+        dataSource.setUrl("jdbc:postgresql://" + host + ":" + port + "/" + database);
+        dataSource.setUsername(username);
+        dataSource.setPassword(password);
 
         return dataSource;
     }
